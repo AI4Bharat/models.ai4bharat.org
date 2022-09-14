8752c9c538d7c0cd54cdda4df1b56f87c1156be3
Umashankar <umashanks99@gmail.com>
2022-09-15 00:17:54 +0530

    Changed Router for Github Pages Support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import ASR from "./pages/ASR/ASR";
@@ -12,12 +12,12 @@ rootElement.style.height = "auto";
 rootElement.style.width = "100%";
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="asr" element={<ASR />} />
         <Route path="xlit" element={<XLIT />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
